f8d1cf8f089bb3c0080664587f0cab167db83d8e
Anas <anashaikh999@gmail.com>
2024-11-01 09:27:01 +0300

    ece2c

--- scripts/deployMalicious.js
+++ scripts/deployMalicious.js
@@ -1,13 +1,15 @@
+require("dotenv").config();
 const { ethers } = require("hardhat");
 
 async function main() {
-    const [deployer] = await ethers.getSigners();
-
-    const BentoBoxAddress = "0x5456070082e58Fa63F0aDacB5f4527da25BD7454"; // Deployed BentoBox address
-    
-
+    // Get the contract factory for MaliciousContract
     const MaliciousContract = await ethers.getContractFactory("MaliciousContract");
-    const maliciousContract = await MaliciousContract.deploy(BentoBoxAddress);
+
+    // Define the address of the BentoBoxV1 contract you want to target
+    const targetContractAddress = "0x5456070082e58Fa63F0aDacB5f4527da25BD7454"; // Update with your target contract address
+
+    // Deploy the MaliciousContract
+    const maliciousContract = await MaliciousContract.deploy(targetContractAddress);
     await maliciousContract.deployed();
 
     console.log("MaliciousContract deployed to:", maliciousContract.address);
